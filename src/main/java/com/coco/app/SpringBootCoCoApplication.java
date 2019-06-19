ff8906eca2887bf2cfb47c3e9af6c17e3e6baa4c
cocoWu <wmc_zd@foxmail.com>
2019-06-19 17:57:38 +0800

    Eclipse 中使用 Git

--- src/main/java/com/coco/app/SpringBootCoCoApplication.java
+++ src/main/java/com/coco/app/SpringBootCoCoApplication.java
@@ -32,9 +32,16 @@ public class SpringBootCoCoApplication {
  * https://github.com/    用户：wmc_zd@foxmail.com
  * 参考网页 ： https://www.cnblogs.com/r360/p/4931432.html
  * 
- * 1. github网站创建新仓库。
+ * 1. github 网站创建新仓库。
  * 
  * 2. 安装Git后，在 GitBash中运行  ssh-keygen -t rsa -C "wmc_zd@foxmail.com"， c/Users/dell/.ssh/id_rsa
  *    参考：https://blog.csdn.net/baidu_30809315/article/details/76687063
  * 
+ * 3. github 网站添加生成的 SSH 公钥
+ * 
+ * 4. Git GUI commit 再强制 push 成功
+ * 
+ * 5. Eclipse 中使用 Git
+ *    参考：https://www.cnblogs.com/wangshouchang/p/6219833.html
+ *    Eclipse 已安装 EGit 插件
  * */
